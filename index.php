338d0bb567c21a2a467d7415ea72f9c911aa560a
Loparev <pavel.loparev.brdm2@gmail.com>
2017-11-11 19:39:16 +0200

    Implemented all Eureka methods

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+$instanceId = 'test_instance_id';
+$availabilityZone = 'test_availability_zone';
+$appName = 'new_app';
+
+$eurekaClient = new EurekaClient('localhost', 8080);
+$eurekaRegistrationData = [
+  'instanceId' => $instanceId,
+  'hostName' => 'test_host_name',
+  'app' => $appName,
+  'ipAddr' => 'test_ip_addr',
+  'port' => [
+    '$' => 80,
+    '@enabled' => 'true',
+  ],
+  'securePort' => [
+    '$' => 443,
+    '@enabled' => 'true',
+  ],
+  'homePageUrl' => 'http://' . $appName,
+  'statusPageUrl' => 'http://' . $appName,
+  'healthCheckUrl' => 'http://' . $appName,
+  'secureHealthCheckUrl' => 'https://' . $appName,
+  'vipAddress' => 'vip_address',
+  'secureVipAddress' => 'secure_vip_address',
+  'metadata' => [
+    'test' => 'test',
+  ],
+  'dataCenterInfo' => [
+    'name' => 'MyOwn',
+    '@class' => 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+  ],
+];
+
+$response = $eurekaClient->register($appName, $eurekaRegistrationData);
+$allApps = $eurekaClient->getAllApps();
+$app = $eurekaClient->getApp($appName);
+$appInstance = $eurekaClient->getAppInstance($appName, $instanceId);
+$instance = $eurekaClient->getInstance($instanceId);
+$response = $eurekaClient->heartBeat($appName, $instanceId);
+$response = $eurekaClient->takeInstanceOut($appName, $instanceId);
+$response = $eurekaClient->putInstanceBack($appName, $instanceId);
+$response = $eurekaClient->updateAppInstanceMetadata($appName, $instanceId, [
+  'test' => 'new_value_1',
+]);
+$instances = $eurekaClient->getInstancesByVipAddress('vip_address');
+$instances = $eurekaClient->getInstancesBySecureVipAddress('secure_vip_address');
+$response = $eurekaClient->deRegister($appName, $instanceId);
+$test = 0;
--- src/EurekaClient.php
+++ src/EurekaClient.php
@@ -1,13 +1,18 @@
 <?php
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
+/**
+ * Class EurekaClient
+ */
 class EurekaClient {
 
   /**
-   * @var string ip
+   * @var string host
    */
-  private $ip;
+  private $host;
 
   /**
    * @var int port
@@ -20,54 +25,224 @@ class EurekaClient {
   private $client;
 
   /**
-   * @var $context
+   * @var string $context
    */
   private $context;
 
   /**
    * @return string
    */
-  private function getEurekaApiUrl() {
-    return $this->ip . ':' . $this->port . '/' . $this->context;
+  private function getEurekaUri() {
+    return $this->host . ':' . $this->port . '/' . $this->context;
   }
 
   /**
    * EurekaClient constructor.
-   * @param $ip
-   * @param $port
-   * @param $context
+   *
+   * @param string $host
+   * @param int $port
+   * @param string $context
    */
-  public function __construct($ip, $port, $context) {
-    $this->ip = $ip;
+  public function __construct($host, $port, $context = 'eureka/v2') {
+    $this->host = $host;
     $this->port = $port;
     $this->context = $context;
     $this->client = new Client([
       'headers' => [
-        'content-type' => 'application/json',
+        'Accept' => 'application/json',
       ],
     ]);
   }
 
   /**
-   * Register service in eureka
-   * @param $appId
-   * @param $data
-   * @return mixed|\Psr\Http\Message\ResponseInterface
+   * Register app in eureka.
+   *
+   * @param string $appId
+   * @param array $data
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
    */
-  public function register($appId, $data) {
-    return $this->client->request('POST', $this->getEurekaApiUrl() . '/apps/' . $appId, [
+  public function register($appId, array $data) {
+    return $this->client->request('POST', $this->getEurekaUri() . '/apps/' . $appId, [
+      'headers' => [
+        'content-type' => 'application/json',
+      ],
       'json' => [
        'instance' => $data,
       ],
     ]);
   }
 
+  /**
+   * De-register app from eureka.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
+   */
   public function deRegister($appId, $instanceId) {
-    return $this->client->request('DELETE', $this->getEurekaApiUrl() . '/apps/' . $appId . '/' . $instanceId);
+    return $this->client->request('DELETE', $this->getEurekaUri() . '/apps/' . $appId . '/' . $instanceId);
   }
 
+  /**
+   * Send app heartbeat.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
+   */
   public function heartBeat($appId, $instanceId) {
-    return $this->client->request('PUT', $this->getEurekaApiUrl() . '/apps/' . $appId . '/' . $instanceId);
+    return $this->client->request('PUT', $this->getEurekaUri() . '/apps/' . $appId . '/' . $instanceId);
+  }
+
+  /**
+   * Get all registered applications.
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getAllApps() {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /apps');
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Get application.
+   *
+   * @param string $appId
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getApp($appId) {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /apps/' . $appId);
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Get application instance.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getAppInstance($appId, $instanceId) {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /apps/' . $appId . '/' . $instanceId);
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Get instance.
+   *
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getInstance($instanceId) {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /instances/' . $instanceId);
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Take instance out of the service.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
+   */
+  public function takeInstanceOut($appId, $instanceId) {
+    return $this->client->request('PUT', $this->getEurekaUri() . '/apps/' . $appId . '/' . $instanceId . '/status', [
+      'query' => [
+        'value' => 'OUT_OF_SERVICE',
+      ],
+    ]);
+  }
+
+  /**
+   * Put instance back into the service.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
+   */
+  public function putInstanceBack($appId, $instanceId) {
+    return $this->client->request('PUT', $this->getEurekaUri() . '/apps/' . $appId . '/' . $instanceId . '/status', [
+      'query' => [
+        'value' => 'UP',
+      ],
+    ]);
+  }
+
+  /**
+   * Update app instance metadata.
+   *
+   * @param string $appId
+   * @param string $instanceId
+   * @param array $metadata
+   *
+   * @throws GuzzleException
+   *
+   * @return ResponseInterface
+   */
+  public function updateAppInstanceMetadata($appId, $instanceId, array $metadata) {
+    return $this->client->request('PUT', $this->getEurekaUri() . '/apps/' . $appId . '/' . $instanceId . '/metadata', [
+      'query' => $metadata,
+    ]);
+  }
+
+  /**
+   * Get all instances by a vip address.
+   *
+   * @param string $vipAddress
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getInstancesByVipAddress($vipAddress) {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /vips/' . $vipAddress);
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
+  }
+
+  /**
+   * Get all instances by a secure vip address.
+   *
+   * @param string $secureVipAddress
+   *
+   * @throws GuzzleException
+   *
+   * @return array
+   */
+  public function getInstancesBySecureVipAddress($secureVipAddress) {
+    $response = $this->client->request('GET', $this->getEurekaUri() . ' /svips/' . $secureVipAddress);
+
+    return \GuzzleHttp\json_decode($response->getBody(), true);
   }
 
 }

a2b1aee428ea0a3e31943823d5e116f2e53e0478
Loparev <pavel.loparev.brdm2@gmail.com>
2017-11-11 21:41:14 +0200

    Added example comments

--- index.php
+++ index.php
@@ -7,21 +7,25 @@ use EurekaClient\Instance\DataCenterInfo;
 
 require_once 'vendor/autoload.php';
 
-$instanceId = 'test_instance_id';
 $appName = 'new_app';
+$instanceId = 'test_instance_id';
 
+// Create app instance metadata.
 $metadata = new Metadata();
 $metadata->set('test_key', 'test_value');
 
+// Create data center metadata.
 $dataCenterMetadata = new Metadata();
 $dataCenterMetadata->set('data_center_test_key', 'data_center_test_value');
 
+// Create data center info (Amazon example).
 $dataCenterInfo = new DataCenterInfo();
 $dataCenterInfo
   ->setName('Amazon')
   ->setClass('com.netflix.appinfo.AmazonInfo')
   ->setMetadata($dataCenterMetadata);
 
+// Create Eureka app instance.
 $instance = new Instance();
 $instance
   ->setInstanceId($instanceId)
@@ -39,18 +43,48 @@ $instance
   ->setMetadata($metadata)
   ->setDataCenterInfo($dataCenterInfo);
 
+// Eureka client usage example.
 $eurekaClient = new EurekaClient('localhost', 8080);
-$response = $eurekaClient->register($appName, $instance);
-$allApps = $eurekaClient->getAllApps();
-$app = $eurekaClient->getApp($appName);
-$appInstance = $eurekaClient->getAppInstance($appName, $instanceId);
-$instance = $eurekaClient->getInstance($instanceId);
-$response = $eurekaClient->heartBeat($appName, $instanceId);
-$response = $eurekaClient->takeInstanceOut($appName, $instanceId);
-$response = $eurekaClient->putInstanceBack($appName, $instanceId);
-$response = $eurekaClient->updateAppInstanceMetadata($appName, $instanceId, [
-  'new_key' => 'new_value',
-]);
-$instances = $eurekaClient->getInstancesByVipAddress('test_vip_address');
-$instances = $eurekaClient->getInstancesBySecureVipAddress('test_secure_vip_address');
-$response = $eurekaClient->deRegister($appName, $instanceId);
+
+try {
+  // Register new application instance.
+  $response = $eurekaClient->register($appName, $instance);
+
+  // Query for all instances.
+  $allApps = $eurekaClient->getAllApps();
+
+  // Query for all application instances.
+  $app = $eurekaClient->getApp($appName);
+
+  // Query for a specific application instance.
+  $appInstance = $eurekaClient->getAppInstance($appName, $instanceId);
+
+  // Query for a specific instance.
+  $instance = $eurekaClient->getInstance($instanceId);
+
+  // Send application instance heartbeat.
+  $response = $eurekaClient->heartBeat($appName, $instanceId);
+
+  // Take instance out of service.
+  $response = $eurekaClient->takeInstanceOut($appName, $instanceId);
+
+  // Put instance back into service.
+  $response = $eurekaClient->putInstanceBack($appName, $instanceId);
+
+  // Update metadata.
+  $response = $eurekaClient->updateAppInstanceMetadata($appName, $instanceId, [
+    'new_key' => 'new_value',
+  ]);
+
+  // Query for all instances under a particular vip address/
+  $instances = $eurekaClient->getInstancesByVipAddress('test_vip_address');
+
+  // Query for all instances under a particular secure vip address.
+  $instances = $eurekaClient->getInstancesBySecureVipAddress('test_secure_vip_address');
+
+  // De-register application instance.
+  $response = $eurekaClient->deRegister($appName, $instanceId);
+}
+catch (Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
